61e5f7d32be6f18c7c70f2f23b53f1933cc10b77
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-10-27 15:09:32 +0530

    Update index.php

--- reader/index.php
+++ reader/index.php
@@ -97,7 +97,7 @@
     <div id="wrapper">
         <div id="dbr-php">
             <h1>
-                Dynamsoft Barcode Reader PHP</h1>
+                Dynamsoft Barcode Reader PHP 1234</h1>
             <!--<form action="">-->               
             <div class="step step1">                
                 <div id="fileUpload" class="getFile">
